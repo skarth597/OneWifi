ac68fc89baf07c5ec2cfe040020e393aa7da1093
MageshRenganathan <Magesh_Renganathan2@comcast.com>
2025-11-07 15:29:20 +0530

    DTMESH-646 [DT][OneWifi] DFS event updates (#626)
    Reason for change: Enable DFS flag for DT platform
    
    Test Procedure: DFS channels should be populated in radio
    state table once booted up
    
    Risks: Low
    
    Priority: P1
    
    Signed-off-by: Magesh Renganathan <Magesh_Renganathan2@comcast.com>

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -6865,6 +6865,9 @@ int wifidb_init_radio_config_default(int radio_index,wifi_radio_operationParam_t
 #if defined (_PP203X_PRODUCT_REQ_)
             cfg.variant = WIFI_80211_VARIANT_A | WIFI_80211_VARIANT_N | WIFI_80211_VARIANT_AC;
             cfg.DfsEnabled = true;
+#elif defined (_GREXT02ACTS_PRODUCT_REQ_)
+	    cfg.variant = WIFI_80211_VARIANT_A | WIFI_80211_VARIANT_N | WIFI_80211_VARIANT_AC | WIFI_80211_VARIANT_AX;
+	    cfg.DfsEnabled = true;
 #else
             cfg.variant = WIFI_80211_VARIANT_A | WIFI_80211_VARIANT_N | WIFI_80211_VARIANT_AC | WIFI_80211_VARIANT_AX;
 #endif
