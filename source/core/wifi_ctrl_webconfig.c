edb84d556f2246f8d485cad69f6ec94135693a9c
Samyuktha Karthikeyan <samyuktha_karthikeyan@comcast.com>
2026-01-22 12:02:08 +0000

    RDKB-36610 Debug
    Reason for change: for onboarding
    Test Procedure: Debug
    Priority: P0
    Risks: Low
    Signed-off-by: Samyuktha Karthikeyan <samyuktha_karthikeyan@comcast.com>

--- source/core/wifi_ctrl_webconfig.c
+++ source/core/wifi_ctrl_webconfig.c
@@ -1700,7 +1700,11 @@ int webconfig_cac_apply(wifi_ctrl_t *ctrl, webconfig_subdoc_decoded_data_t *data
         for (vap_index = 0; vap_index < getNumberVAPsPerRadio(radio_index); vap_index++) {
             wifi_util_dbg_print(WIFI_CTRL,"Comparing cac config \n");
             wifi_util_dbg_print(WIFI_CTRL,"Comparing cac config for Radio Index: %d Vap Index: %d \n",radio_index,vap_index);
-
+           // to run cac only for hotspot vaps
+            if(isVapHotspot(vap_index))
+            {
+                wifi_util_dbg_print(WIFI_CTRL,"Vap index : %d is hotspot vap running cac \n",vap_index);
+            }
             if (is_preassoc_cac_config_changed(&l_vap_maps->vap_array[vap_index], &data->radios[radio_index].vaps.vap_map.vap_array[vap_index])
                 || is_postassoc_cac_config_changed(&l_vap_maps->vap_array[vap_index], &data->radios[radio_index].vaps.vap_map.vap_array[vap_index])) {
                 // cac or tcm data changed apply
