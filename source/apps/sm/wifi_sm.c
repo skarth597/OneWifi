90bd219eb8057250ffb7097eb2ea96363a5dd485
Samyuktha Karthikeyan <samyuktha_karthikeyan@comcast.com>
2026-05-19 06:07:33 +0000

    RDKB-63687 Code development to reduce SM app memory usage
    Reason for change: To reduce the SM app memory usage
    Test Procedure: Debug
    Priority: P1
    Risks: Low
    Signed-off-by: Samyuktha Karthikeyan <samyuktha_karthikeyan@comcast.com>

--- source/apps/sm/wifi_sm.c
+++ source/apps/sm/wifi_sm.c
@@ -43,7 +43,7 @@ typedef struct {
     unsigned int    req_stats_vap_mask;
 } client_assoc_stats_t;
 
-client_assoc_stats_t client_assoc_stats[MAX_NUM_RADIOS];
+client_assoc_stats_t *client_assoc_stats = NULL; //allocate during app init
 
 int sm_survey_type_conversion(wifi_neighborScanMode_t *halw_scan_type, survey_type_t *app_stat_type, unsigned int conv_type)
 {
@@ -796,6 +796,7 @@ int sm_event(wifi_app_t *app, wifi_event_t *event)
 int sm_init(wifi_app_t *app, unsigned int create_flag)
 {
     int rc = RETURN_OK;
+    int radios_count = getNumberRadios();
     if (app_init(app, create_flag) != 0) {
         return RETURN_ERR;
     }
@@ -805,8 +806,23 @@ int sm_init(wifi_app_t *app, unsigned int create_flag)
     app->data.u.sm_data.sm_stats_config_map  = hash_map_create();
     app->data.u.sm_data.report_tasks_map  = hash_map_create();
 
-    memset(client_assoc_stats, 0, sizeof(client_assoc_stats));
+    client_assoc_stats = calloc(radios_count, sizeof(client_assoc_stats_t));
+    if (client_assoc_stats == NULL) {
+        wifi_util_error_print(WIFI_SM, "%s:%d: calloc failed for client_assoc_stats\n", __func__,
+            __LINE__);
+        hash_map_destroy(app->data.u.sm_data.report_tasks_map);
+        app->data.u.sm_data.report_tasks_map = NULL;
+        free_sm_stats_config_map(app);
+        return RETURN_ERR;
+    }
     rc = sm_report_init(app);
+    if (rc != RETURN_OK) {
+        free(client_assoc_stats);
+        client_assoc_stats = NULL;
+        hash_map_destroy(app->data.u.sm_data.report_tasks_map);
+        app->data.u.sm_data.report_tasks_map = NULL;
+        free_sm_stats_config_map(app);
+    }
 
     wifi_util_info_print(WIFI_SM, "%s:%d: Init SM app %s\n", __func__, __LINE__, rc ? "failure" : "success");
 
@@ -838,7 +854,10 @@ int free_sm_stats_config_map(wifi_app_t *app)
 int sm_deinit(wifi_app_t *app)
 {
     sm_stats_to_monitor_set(app, false);
-    free_sm_stats_config_map(app);
+    if (client_assoc_stats != NULL) {
+        free(client_assoc_stats);
+        client_assoc_stats = NULL;
+    }
     sm_report_deinit(app);
     return RETURN_OK;
 }
