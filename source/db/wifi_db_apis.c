e061af772223d788d4234f5959616f4c9aed3d67
Samyuktha Karthikeyan <samyuktha_karthikeyan@comcast.com>
2026-09-01 05:41:58 +0000

    RDKB-36610 Debug
    Reason for change: for onboarding
    Test Procedure: Debug
    Priority: P0
    Risks: Low
    Signed-off-by: Samyuktha Karthikeyan <samyuktha_karthikeyan@comcast.com>

--- source/db/wifi_db_apis.c
+++ source/db/wifi_db_apis.c
@@ -5138,7 +5138,8 @@ static void wifidb_vap_config_upgrade(wifi_vap_info_map_t *config, rdk_wifi_vap_
         }
 
 #if defined(_SR213_PRODUCT_REQ_) || defined(_WNXL11BWL_PRODUCT_REQ_) || \
-    defined(_SCXF11BFL_PRODUCT_REQ_)
+    defined(_SCXF11BFL_PRODUCT_REQ_) || defined(_XB7_PRODUCT_REQ_) || defined(_XB8_PRODUCT_REQ_) || \
+    defined(_XB10_PRODUCT_REQ_) || defined(_SCER11BEL_PRODUCT_REQ_) || defined(_CBR2_PRODUCT_REQ_)
         if (g_wifidb->db_version < ONEWIFI_DB_VERSION_HOSTAP_MGMT_FRAME_CTRL_NEW_FLAG) {
             if (!isVapSTAMesh(config->vap_array[i].vap_index)) {
                 config->vap_array[i].u.bss_info.hostap_mgt_frame_ctrl = true;
