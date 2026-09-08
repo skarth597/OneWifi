813a3940679874c76f844efd23233695f4c4e401
Mothi Shree M J <mothishree_mallaiyanjothimani@comcast.com>
2026-09-08 11:21:01 +0530

    RDKB-66589: [Auto][Sprint] Observed onewifi crash during wifi sanity script execution [App Name: OneWifi] [Fingerprint:68810161](#1410)
    Reason for change: Fixed an out-of-bounds array access in Stats3_GetParamIntValue where post-incrementing the loop index inside the while condition caused chan_data[] to be accessed at index = num_channels.
    Test Procedure:
    
    Run the wifi sanity script execution suite.
    Verify that Stats3_GetParamIntValue executes without encountering a crash or segmentation fault when reading channel statistics.
    Priority: P1
    Risks: Low
    Signed-off-by: mothishree_mallaiyanjothimani@comcast.com

--- source/dml/tr_181/ml/cosa_wifi_dml.c
+++ source/dml/tr_181/ml/cosa_wifi_dml.c
@@ -4630,7 +4630,7 @@ Stats3_GetParamIntValue
         pthread_mutex_lock(&monitor_param->data_lock);
         radio_activity_factor =
             monitor_param->radio_data[instance_number].RadioActivityFactor;
-        while ((i++) < monitor_param->radio_chan_stats_data[instance_number]
+        while (i < monitor_param->radio_chan_stats_data[instance_number]
                            .num_channels) {
             utilization_rx +=
                 monitor_param->radio_chan_stats_data[instance_number]
@@ -4640,6 +4640,7 @@ Stats3_GetParamIntValue
                 monitor_param->radio_chan_stats_data[instance_number]
                     .chan_data[i]
                     .ch_utilization_busy_tx;
+            i++;
         }
         pthread_mutex_unlock(&monitor_param->data_lock);
         utilization_total = utilization_rx + utilization_tx;
