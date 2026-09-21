18594e70d67ceae6bdee8db2877a8cabd68105da
yashu <yakshagowda37@gmail.com>
2026-09-21 23:26:49 +0530

    RDKBDEV-3494: Fix resource leak in Passpoint_SetParamStringValue (#1408)
    Problem:
    In Passpoint_SetParamStringValue(), the cJSON object created by
    cJSON_Parse() is not deleted on the successful return path.
    
    Fix:
    Add cJSON_Delete(p_root) before returning TRUE to release the
    parsed JSON object.
    
    Signed-off-by: Yashodha KB <yakshagowda37@gmail.com>
    Co-authored-by: Narendra Varma Dandu <narendandu@gmail.com>

--- source/dml/tr_181/ml/cosa_wifi_dml.c
+++ source/dml/tr_181/ml/cosa_wifi_dml.c
@@ -20906,6 +20906,7 @@ Passpoint_SetParamStringValue
                 cJSON_Delete(p_root);
                 return FALSE;
             }
+            cJSON_Delete(p_root);
 	    set_dml_cache_vap_config_changed(instance_number - 1);
             return TRUE;
         }
