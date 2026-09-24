d14f713b9532e6cb87164f07c09dace398f0c0a0
Vitaliy Lyashenko <36844479+vfrvtlk@users.noreply.github.com>
2026-09-24 17:30:31 +0300

    RDKB-66205: [MLO] Add telemetry markers and reporting support for MLO client adoption, latency, connectivity, GMC, and reboot impact (#1322) (#1458)
    Reason for change: Addition of related MLO clients metrics
    1) total number of MLO devices connected on each configured MLD
    2) Number of nLink connected devices
    
    Test Procedure: Connect MLO/Non MLO clients and verify if metrics are
    correctly printed into rdklogs/logs/wifihealth.txt
    
    Risks: Low
    Priority: P1
    
    Signed-off-by: Vitaliy Lyashenko <v.for.vitalik@gmail.com>
    Co-authored-by: Narendra Varma Dandu <narendandu@gmail.com>

--- source/core/wifi_ctrl.h
+++ source/core/wifi_ctrl.h
@@ -441,6 +441,7 @@ void update_mlo_rfc_enable(bool init);
 
 wifi_vap_info_t *get_mlo_partner_link_by_link_id(wifi_vap_info_t *vapInfo, UINT link_id);
 wifi_mld_common_info_t *get_mld_from_vap_info(wifi_vap_info_t *vap);
+void update_apmld_map(apmld_map_t *apmld_map);
 
 #ifdef __cplusplus
 }
